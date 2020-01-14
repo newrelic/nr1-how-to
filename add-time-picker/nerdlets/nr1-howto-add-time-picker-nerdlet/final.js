afc4d171e918778d4b6312627a13c9a95d845165
Jemiah Sius <jsius@newrelic.com>
2020-01-14 09:00:20 -0800

    initial commit for add-time-picker

---
+++ add-time-picker/nerdlets/nr1-howto-add-time-picker-nerdlet/final.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import {
+  Grid,
+  GridItem,
+  HeadingText,
+  AreaChart,
+  TableChart,
+  PieChart
+} from 'nr1';
+
+// https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
+
+export default class Nr1HowtoAddTimePicker extends React.Component {
+    constructor(props){
+        super(props)
+        this.accountId = 1;
+    }
+    render() {
+        const ttlTrx = `FROM Transaction SELECT count(*) COMPARE WITH 1 week ago SINCE 1 hour ago`;
+        const avgResTime = `SELECT average(duration) FROM Transaction FACET appName since 1 hour ago TIMESERIES AUTO`;
+        const trxOverview = `FROM Transaction SELECT count(*) as 'Transactions', apdex(duration) as 'apdex', percentile(duration, 99, 95) FACET appName SINCE 1 hour ago`;
+        return (
+            <>
+            <Grid
+            className="primary-grid"
+            spacingType={[Grid.SPACING_TYPE.NONE, Grid.SPACING_TYPE.NONE]}
+            >
+                <GridItem className="primary-content-container" columnSpan={6}>
+                    <main className="primary-content full-height">
+                    <HeadingText spacingType={[HeadingText.SPACING_TYPE.MEDIUM]} type={HeadingText.TYPE.HEADING_4}>
+                        Transaction Overview
+                    </HeadingText>
+                    <TableChart fullWidth accountId={this.accountId} query={trxOverview} />
+                    </main>
+                </GridItem>
+                <GridItem className="primary-content-container" columnSpan={6}>
+                    <main className="primary-content full-height">
+                    <HeadingText spacingType={[HeadingText.SPACING_TYPE.MEDIUM]} type={HeadingText.TYPE.HEADING_4}>
+                        Average Response Time
+                    </HeadingText>
+                    <AreaChart fullWidth accountId={this.accountId} query={avgResTime} />
+                    </main>
+                </GridItem>
+                <GridItem className="primary-content-container" columnSpan={6}>
+                    <main className="primary-content full-height">
+                    <HeadingText spacingType={[HeadingText.SPACING_TYPE.MEDIUM]} type={HeadingText.TYPE.HEADING_4}>
+                        Response Code
+                    </HeadingText>
+                    <PieChart fullWidth accountId={this.accountId} query={responseCodes} />
+                    </main>
+                </GridItem>
+                <GridItem className="primary-content-container" columnSpan={6}>
+                    <main className="primary-content full-height">
+                    <HeadingText spacingType={[HeadingText.SPACING_TYPE.MEDIUM]} type={HeadingText.TYPE.HEADING_4}>
+                        Transaction Errors
+                    </HeadingText>
+                    <PieChart fullWidth accountId={this.accountId} query={errCount} />
+                    </main>
+                </GridItem>
+            </Grid>
+        </>
+        );
+    }
+}
