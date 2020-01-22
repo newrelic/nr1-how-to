82b3c40f1afda3570555a4758fa6779c3f270309
Jemiah Sius <jsius@newrelic.com>
2020-01-22 11:36:56 -0600

    Updated add-time picker with how-to.md file included

--- add-time-picker/nerdlets/nr1-howto-add-time-picker-nerdlet/final.js
+++ add-time-picker/nerdlets/nr1-howto-add-time-picker-nerdlet/final.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {
+  PlatformStateContext,
   Grid,
   GridItem,
   HeadingText,
@@ -7,6 +8,7 @@ import {
   TableChart,
   PieChart
 } from 'nr1';
+import { timeRangeToNrql } from '@newrelic/nr1-community';
 
 // https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
 
@@ -16,49 +18,63 @@ export default class Nr1HowtoAddTimePicker extends React.Component {
         this.accountId = 1;
     }
     render() {
-        const ttlTrx = `FROM Transaction SELECT count(*) COMPARE WITH 1 week ago SINCE 1 hour ago`;
-        const avgResTime = `SELECT average(duration) FROM Transaction FACET appName since 1 hour ago TIMESERIES AUTO`;
-        const trxOverview = `FROM Transaction SELECT count(*) as 'Transactions', apdex(duration) as 'apdex', percentile(duration, 99, 95) FACET appName SINCE 1 hour ago`;
+        const avgResTime = `SELECT average(duration) FROM Transaction FACET appName TIMESERIES AUTO `;
+        const trxOverview = `FROM Transaction SELECT count(*) as 'Transactions', apdex(duration) as 'apdex', percentile(duration, 99, 95) FACET appName `;
+        const errCount = `FROM TransactionError SELECT count(*) as 'Transaction Errors' FACET error.message `;
+        const responseCodes = `SELECT count(*) as 'Response Code' FROM Transaction FACET httpResponseCode `;
+
         return (
-            <>
-            <Grid
-            className="primary-grid"
-            spacingType={[Grid.SPACING_TYPE.NONE, Grid.SPACING_TYPE.NONE]}
-            >
-                <GridItem className="primary-content-container" columnSpan={6}>
-                    <main className="primary-content full-height">
-                    <HeadingText spacingType={[HeadingText.SPACING_TYPE.MEDIUM]} type={HeadingText.TYPE.HEADING_4}>
-                        Transaction Overview
-                    </HeadingText>
-                    <TableChart fullWidth accountId={this.accountId} query={trxOverview} />
-                    </main>
-                </GridItem>
-                <GridItem className="primary-content-container" columnSpan={6}>
-                    <main className="primary-content full-height">
-                    <HeadingText spacingType={[HeadingText.SPACING_TYPE.MEDIUM]} type={HeadingText.TYPE.HEADING_4}>
-                        Average Response Time
-                    </HeadingText>
-                    <AreaChart fullWidth accountId={this.accountId} query={avgResTime} />
-                    </main>
-                </GridItem>
-                <GridItem className="primary-content-container" columnSpan={6}>
-                    <main className="primary-content full-height">
-                    <HeadingText spacingType={[HeadingText.SPACING_TYPE.MEDIUM]} type={HeadingText.TYPE.HEADING_4}>
-                        Response Code
-                    </HeadingText>
-                    <PieChart fullWidth accountId={this.accountId} query={responseCodes} />
-                    </main>
-                </GridItem>
-                <GridItem className="primary-content-container" columnSpan={6}>
-                    <main className="primary-content full-height">
-                    <HeadingText spacingType={[HeadingText.SPACING_TYPE.MEDIUM]} type={HeadingText.TYPE.HEADING_4}>
-                        Transaction Errors
-                    </HeadingText>
-                    <PieChart fullWidth accountId={this.accountId} query={errCount} />
-                    </main>
-                </GridItem>
-            </Grid>
-        </>
+            <PlatformStateContext.Consumer>
+                {(PlatformState) => {
+                    /* Taking a peek at the PlatformState */
+                    console.log(PlatformState)
+
+                    const since = timeRangeToNrql(PlatformState);
+                    console.log(since);
+
+                    return (
+                    <>
+                        <Grid
+                        className="primary-grid"
+                        spacingType={[Grid.SPACING_TYPE.NONE, Grid.SPACING_TYPE.NONE]}
+                        >
+                            <GridItem className="primary-content-container" columnSpan={6}>
+                                <main className="primary-content full-height">
+                                <HeadingText spacingType={[HeadingText.SPACING_TYPE.MEDIUM]} type={HeadingText.TYPE.HEADING_4}>
+                                    Transaction Overview
+                                </HeadingText>
+                                <TableChart fullWidth accountId={this.accountId} query={trxOverview+since} />
+                                </main>
+                            </GridItem>
+                            <GridItem className="primary-content-container" columnSpan={6}>
+                                <main className="primary-content full-height">
+                                <HeadingText spacingType={[HeadingText.SPACING_TYPE.MEDIUM]} type={HeadingText.TYPE.HEADING_4}>
+                                    Average Response Time
+                                </HeadingText>
+                                <AreaChart fullWidth accountId={this.accountId} query={avgResTime+since} />
+                                </main>
+                            </GridItem>
+                            <GridItem className="primary-content-container" columnSpan={6}>
+                                <main className="primary-content full-height">
+                                <HeadingText spacingType={[HeadingText.SPACING_TYPE.MEDIUM]} type={HeadingText.TYPE.HEADING_4}>
+                                    Response Code
+                                </HeadingText>
+                                <PieChart fullWidth accountId={this.accountId} query={responseCodes+since} />
+                                </main>
+                            </GridItem>
+                            <GridItem className="primary-content-container" columnSpan={6}>
+                                <main className="primary-content full-height">
+                                <HeadingText spacingType={[HeadingText.SPACING_TYPE.MEDIUM]} type={HeadingText.TYPE.HEADING_4}>
+                                    Transaction Errors
+                                </HeadingText>
+                                <PieChart fullWidth accountId={this.accountId} query={errCount+since} />
+                                </main>
+                            </GridItem>
+                        </Grid>
+                    </>
+                    );
+                }}
+            </PlatformStateContext.Consumer>
         );
     }
 }
--- add-time-picker/nerdlets/nr1-howto-add-time-picker-nerdlet/index.js
+++ add-time-picker/nerdlets/nr1-howto-add-time-picker-nerdlet/index.js
@@ -4,67 +4,74 @@ import {
   GridItem,
   HeadingText,
   AreaChart,
-  BillboardChart,
-  TableChart, 
-  PieChart
+  TableChart,
+  PieChart,
+  PlatformStateContext
 } from 'nr1';
+import { timeRangeToNrql } from '@newrelic/nr1-community';
+
 
 // https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
 
 export default class Nr1HowtoAddTimePicker extends React.Component {
     constructor(props){
         super(props)
-        this.accountId = 1;
+        this.accountId = 1606862;
     }
     render() {
-        const ttlTrx = `FROM Transaction SELECT count(*) COMPARE WITH 1 week ago SINCE 1 hour ago`;
-        const avgResTime = `SELECT average(duration) FROM Transaction FACET appName since 1 hour ago TIMESERIES AUTO`;
-        const trxOverview = `FROM Transaction SELECT count(*) as 'Transactions', apdex(duration) as 'apdex', percentile(duration, 99, 95) FACET appName SINCE 1 hour ago`;
-        
-        
-        
-        const errCount = `FROM TransactionError SELECT count(*) as 'Transaction Errors' FACET error.message SINCE 1 hour ago`;
-        const responseCodes = `SELECT count(*) as 'Response Code' FROM Transaction since 1 hour ago FACET httpResponseCode`;
+        const avgResTime = `SELECT average(duration) FROM Transaction FACET appName TIMESERIES AUTO`;
+        const trxOverview = `FROM Transaction SELECT count(*) as 'Transactions', apdex(duration) as 'apdex', percentile(duration, 99, 95) FACET appName`;
+        const errCount = `FROM TransactionError SELECT count(*) as 'Transaction Errors' FACET error.message`;
+        const responseCodes = `SELECT count(*) as 'Response Code' FROM Transaction FACET httpResponseCode`;
         return (
-            <>
-            <Grid
-            className="primary-grid"
-            spacingType={[Grid.SPACING_TYPE.NONE, Grid.SPACING_TYPE.NONE]}
-            >
-                <GridItem className="primary-content-container" columnSpan={6}>
-                    <main className="primary-content full-height">
-                    <HeadingText spacingType={[HeadingText.SPACING_TYPE.MEDIUM]} type={HeadingText.TYPE.HEADING_4}>
-                        Transaction Overview
-                    </HeadingText>
-                    <TableChart fullWidth accountId={this.accountId} query={trxOverview} />
-                    </main>
-                </GridItem>
-                <GridItem className="primary-content-container" columnSpan={6}>
-                    <main className="primary-content full-height">
-                    <HeadingText spacingType={[HeadingText.SPACING_TYPE.MEDIUM]} type={HeadingText.TYPE.HEADING_4}>
-                        Average Response Time
-                    </HeadingText>
-                    <AreaChart fullWidth accountId={this.accountId} query={avgResTime} />
-                    </main>
-                </GridItem>
-                <GridItem className="primary-content-container" columnSpan={6}>
-                    <main className="primary-content full-height">
-                    <HeadingText spacingType={[HeadingText.SPACING_TYPE.MEDIUM]} type={HeadingText.TYPE.HEADING_4}>
-                        Response Code 
-                    </HeadingText>
-                    <PieChart fullWidth accountId={this.accountId} query={responseCodes} />
-                    </main>
-                </GridItem>
-                <GridItem className="primary-content-container" columnSpan={6}>
-                    <main className="primary-content full-height">
-                    <HeadingText spacingType={[HeadingText.SPACING_TYPE.MEDIUM]} type={HeadingText.TYPE.HEADING_4}>
-                        Transaction Errors
-                    </HeadingText>
-                    <PieChart fullWidth accountId={this.accountId} query={errCount} />
-                    </main>
-                </GridItem>
-            </Grid>
-        </>
+            <PlatformStateContext.Consumer>
+                {(PlatformState) => {
+
+                    const since = timeRangeToNrql(PlatformState);
+
+                    return (
+                        <>
+                            <Grid
+                            className="primary-grid"
+                            spacingType={[Grid.SPACING_TYPE.NONE, Grid.SPACING_TYPE.NONE]}
+                            >
+                                <GridItem className="primary-content-container" columnSpan={6}>
+                                    <main className="primary-content full-height">
+                                    <HeadingText spacingType={[HeadingText.SPACING_TYPE.MEDIUM]} type={HeadingText.TYPE.HEADING_4}>
+                                        Transaction Overview
+                                    </HeadingText>
+                                    <TableChart fullWidth accountId={this.accountId} query={trxOverview+" "+since} />
+                                    </main>
+                                </GridItem>
+                                <GridItem className="primary-content-container" columnSpan={6}>
+                                    <main className="primary-content full-height">
+                                    <HeadingText spacingType={[HeadingText.SPACING_TYPE.MEDIUM]} type={HeadingText.TYPE.HEADING_4}>
+                                        Average Response Time
+                                    </HeadingText>
+                                    <AreaChart fullWidth accountId={this.accountId} query={avgResTime+" "+since} />
+                                    </main>
+                                </GridItem>
+                                <GridItem className="primary-content-container" columnSpan={6}>
+                                    <main className="primary-content full-height">
+                                    <HeadingText spacingType={[HeadingText.SPACING_TYPE.MEDIUM]} type={HeadingText.TYPE.HEADING_4}>
+                                        Response Code
+                                    </HeadingText>
+                                    <PieChart fullWidth accountId={this.accountId} query={responseCodes+" "+since} />
+                                    </main>
+                                </GridItem>
+                                <GridItem className="primary-content-container" columnSpan={6}>
+                                    <main className="primary-content full-height">
+                                    <HeadingText spacingType={[HeadingText.SPACING_TYPE.MEDIUM]} type={HeadingText.TYPE.HEADING_4}>
+                                        Transaction Errors
+                                    </HeadingText>
+                                    <PieChart fullWidth accountId={this.accountId} query={errCount+" "+since} />
+                                    </main>
+                                </GridItem>
+                            </Grid>
+                        </>
+                    )
+                }}
+            </PlatformStateContext.Consumer>
         );
     }
 }
