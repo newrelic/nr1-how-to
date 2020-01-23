44e932a85fcba337a6e1bc85af4dbf6e5f2731c6
Jemiah Sius <jsius@newrelic.com>
2020-01-22 21:34:34 -0600

    reset index.js file

--- add-time-picker/nerdlets/nr1-howto-add-time-picker-nerdlet/index.js
+++ add-time-picker/nerdlets/nr1-howto-add-time-picker-nerdlet/index.js
@@ -5,18 +5,15 @@ import {
   HeadingText,
   AreaChart,
   TableChart,
-  PieChart,
-  PlatformStateContext
+  PieChart
 } from 'nr1';
-import { timeRangeToNrql } from '@newrelic/nr1-community';
-
 
 // https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
 
 export default class Nr1HowtoAddTimePicker extends React.Component {
     constructor(props){
         super(props)
-        this.accountId = 1606862;
+        this.accountId = "<ADD YOUR ACCOUNT ID>";
     }
     render() {
         const avgResTime = `SELECT average(duration) FROM Transaction FACET appName TIMESERIES AUTO`;
@@ -24,54 +21,45 @@ export default class Nr1HowtoAddTimePicker extends React.Component {
         const errCount = `FROM TransactionError SELECT count(*) as 'Transaction Errors' FACET error.message`;
         const responseCodes = `SELECT count(*) as 'Response Code' FROM Transaction FACET httpResponseCode`;
         return (
-            <PlatformStateContext.Consumer>
-                {(PlatformState) => {
-
-                    const since = timeRangeToNrql(PlatformState);
-
-                    return (
-                        <>
-                            <Grid
-                            className="primary-grid"
-                            spacingType={[Grid.SPACING_TYPE.NONE, Grid.SPACING_TYPE.NONE]}
-                            >
-                                <GridItem className="primary-content-container" columnSpan={6}>
-                                    <main className="primary-content full-height">
-                                    <HeadingText spacingType={[HeadingText.SPACING_TYPE.MEDIUM]} type={HeadingText.TYPE.HEADING_4}>
-                                        Transaction Overview
-                                    </HeadingText>
-                                    <TableChart fullWidth accountId={this.accountId} query={trxOverview+" "+since} />
-                                    </main>
-                                </GridItem>
-                                <GridItem className="primary-content-container" columnSpan={6}>
-                                    <main className="primary-content full-height">
-                                    <HeadingText spacingType={[HeadingText.SPACING_TYPE.MEDIUM]} type={HeadingText.TYPE.HEADING_4}>
-                                        Average Response Time
-                                    </HeadingText>
-                                    <AreaChart fullWidth accountId={this.accountId} query={avgResTime+" "+since} />
-                                    </main>
-                                </GridItem>
-                                <GridItem className="primary-content-container" columnSpan={6}>
-                                    <main className="primary-content full-height">
-                                    <HeadingText spacingType={[HeadingText.SPACING_TYPE.MEDIUM]} type={HeadingText.TYPE.HEADING_4}>
-                                        Response Code
-                                    </HeadingText>
-                                    <PieChart fullWidth accountId={this.accountId} query={responseCodes+" "+since} />
-                                    </main>
-                                </GridItem>
-                                <GridItem className="primary-content-container" columnSpan={6}>
-                                    <main className="primary-content full-height">
-                                    <HeadingText spacingType={[HeadingText.SPACING_TYPE.MEDIUM]} type={HeadingText.TYPE.HEADING_4}>
-                                        Transaction Errors
-                                    </HeadingText>
-                                    <PieChart fullWidth accountId={this.accountId} query={errCount+" "+since} />
-                                    </main>
-                                </GridItem>
-                            </Grid>
-                        </>
-                    )
-                }}
-            </PlatformStateContext.Consumer>
+            <>
+            <Grid
+            className="primary-grid"
+            spacingType={[Grid.SPACING_TYPE.NONE, Grid.SPACING_TYPE.NONE]}
+            >
+            <GridItem className="primary-content-container" columnSpan={6}>
+                <main className="primary-content full-height">
+                <HeadingText spacingType={[HeadingText.SPACING_TYPE.MEDIUM]} type={HeadingText.TYPE.HEADING_4}>
+                    Transaction Overview
+                </HeadingText>
+                <TableChart fullWidth accountId={this.accountId} query={trxOverview} />
+                </main>
+            </GridItem>
+            <GridItem className="primary-content-container" columnSpan={6}>
+                <main className="primary-content full-height">
+                <HeadingText spacingType={[HeadingText.SPACING_TYPE.MEDIUM]} type={HeadingText.TYPE.HEADING_4}>
+                    Average Response Time
+                </HeadingText>
+                <AreaChart fullWidth accountId={this.accountId} query={avgResTime} />
+                </main>
+            </GridItem>
+            <GridItem className="primary-content-container" columnSpan={6}>
+                <main className="primary-content full-height">
+                <HeadingText spacingType={[HeadingText.SPACING_TYPE.MEDIUM]} type={HeadingText.TYPE.HEADING_4}>
+                    Response Code
+                </HeadingText>
+                <PieChart fullWidth accountId={this.accountId} query={responseCodes} />
+                </main>
+            </GridItem>
+            <GridItem className="primary-content-container" columnSpan={6}>
+                <main className="primary-content full-height">
+                <HeadingText spacingType={[HeadingText.SPACING_TYPE.MEDIUM]} type={HeadingText.TYPE.HEADING_4}>
+                    Transaction Errors
+                </HeadingText>
+                <PieChart fullWidth accountId={this.accountId} query={errCount} />
+                </main>
+            </GridItem>
+        </Grid>
+        </>
         );
     }
 }
