ce03368a00f1d65eed7d92987b068ad9b8321c19
Jemiah Sius <jsius@newrelic.com>
2020-04-06 10:59:18 -0400

    reset index to starting point

--- create-a-table/nerdlets/create-a-table-nerdlet/index.js
+++ create-a-table/nerdlets/create-a-table-nerdlet/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  PlatformStateContext, Grid, GridItem, HeadingText, AreaChart, TableChart, Table, TableHeader, TableHeaderCell, TableRow, TableRowCell, EntityTitleTableRowCell, MetricTableRowCell
+  PlatformStateContext, Grid, GridItem, HeadingText, AreaChart, TableChart,
 } from 'nr1';
 import { timeRangeToNrql } from '@newrelic/nr1-community';
 
@@ -9,58 +9,7 @@ import { timeRangeToNrql } from '@newrelic/nr1-community';
 export default class Nr1HowtoAddTimePicker extends React.Component {
     constructor(props){
         super(props)
-        this.accountId = 1606862;
-    }
-
-    _getItems() {
-        return [
-            {
-                team: 'Backend',
-                company: 'Comtest',
-                name: 'Web Portal',
-                alertSeverity: 'CRITICAL',
-                reporting: true,
-                value: 0.9202394,
-                commit: '0f58ef',
-            },
-            {
-                team: 'Frontend',
-                company: 'Comtest',
-                name: 'Promo Service',
-                alertSeverity: 'CRITICAL',
-                reporting: true,
-                value: 0.9123988,
-                commit: 'e10fb3',
-            },
-            {
-                team: 'DB',
-                company: 'Comtest',
-                name: 'Tower Portland',
-                alertSeverity: 'CRITICAL',
-                reporting: true,
-                value: 0.82331,
-                commit: 'ff8b07a',
-            },
-        ];
-    }
-
-    _getActions() {
-        return [
-            {
-                label: 'Alert Team',
-                iconType: TableRow.ACTIONS_ICON_TYPE.INTERFACE__OPERATIONS__ALERT,
-                onClick: (evt, { item, index }) => {
-                    alert(`Alert Team: ${item.team}`);
-                },
-            },
-            {
-                label: 'Rollback Version',
-                iconType: TableRow.ACTIONS_ICON_TYPE.INTERFACE__OPERATIONS__UNDO,
-                onClick: (evt, { item, index }) => {
-                    alert(`Rollback from: ${item.commit}`);
-                },
-            },
-        ];
+        this.accountId = REPLACE ME;
     }
 
     render() {
@@ -96,24 +45,7 @@ export default class Nr1HowtoAddTimePicker extends React.Component {
                             </GridItem>
                             <GridItem className="primary-content-container" columnSpan={12}>
                             {/*  Build table here. */}
-                            <Table items={this._getItems()}>
-                                <TableHeader>
-                                    <TableHeaderCell>Application</TableHeaderCell>
-                                    <TableHeaderCell>Company</TableHeaderCell>
-                                    <TableHeaderCell>Team</TableHeaderCell>
-                                    <TableHeaderCell>Commit</TableHeaderCell>
-                                    <TableHeaderCell>Apdex</TableHeaderCell>
-                                </TableHeader>
-                                {({ item }) => (
-                                    <TableRow actions={this._getActions()}>
-                                        <EntityTitleTableRowCell value={item}/>
-                                        <TableRowCell>{item.company}</TableRowCell>
-                                        <TableRowCell>{item.team}</TableRowCell>
-                                        <TableRowCell>{item.commit}</TableRowCell>
-                                        <MetricTableRowCell type={MetricTableRowCell.TYPE.APDEX} value={item.value} />
-                                    </TableRow>
-                                )}
-                            </Table>
+
                             </GridItem>
                         </Grid>
                     </>
